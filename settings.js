cbb226f18d7272b5747a242a92de552c41888e27
favourdre2008 <favourdre2008@gmail.com>
2024-09-30 12:31:02 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2348026743946"
+global.ownername = "DRÃY"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2348026743946'  //creator number
+global.ownername = 'DRÃY' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
